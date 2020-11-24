590de14cc9f7a94a779b9d444ac63a578392ab4b
Tom Liu <tom.5610@gmail.com>
2020-11-24 01:59:43 +0000

    Add experiments and HPO notification.

--- notebook/code/query_hpo_job.py
+++ notebook/code/query_hpo_job.py
@@ -19,12 +19,18 @@ def lambda_handler(event, context):
     #Query boto3 API to check training status.
     response = sm_client.describe_hyper_parameter_tuning_job(HyperParameterTuningJobName = hpo_job_name)
     best_training_job = response['BestTrainingJob']
-    response = sm.describe_training_job(TrainingJobName = best_training_job['TrainingJobName'])
+    final_hpo_objective_metric = best_training_job['FinalHyperParameterTuningJobObjectiveMetric']
+
+    response = sm_client.describe_training_job(TrainingJobName = best_training_job['TrainingJobName'])
     logger.info("HPO Job:{} 's best training job':{}.".format(hpo_job_name, best_training_job['TrainingJobName']))
+    
+
 
     return {
         'statusCode': 200,
         'bestTrainingJob': {
+            'trainingJobName': response['TrainingJobName'],
+            'finalHyperParameterTuningJobObjectiveMetric': final_hpo_objective_metric,
             'modelArtifacts': response['ModelArtifacts'],
             'hyperParameters': response['HyperParameters']
         }
