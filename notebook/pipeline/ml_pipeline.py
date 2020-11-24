1066736148d6505ccb5f99061b65855b2bfa9c3d
tom5610 <tom.5610@gmail.com>
2020-11-24 21:49:02 +1100

    initial ml pipeline code.

---
+++ notebook/pipeline/ml_pipeline.py
@@ -0,0 +1,539 @@
+from ml_pipeline_dependencies import *
+
+processing_output_data = f"s3://{bucket_name}/preprocessing/output"
+PREPROCESSING_SCRIPT_LOCATION = "./pipeline/preprocessing.py"
+suffix = datetime.now().strftime("%y%m%d-%H%M")
+
+def upload_preprocess_code(bucket_name):
+    input_code_uri = sagemaker_session.upload_data(
+        PREPROCESSING_SCRIPT_LOCATION,
+        bucket = bucket_name,
+        key_prefix = "preprocessing/code",
+    )
+    return input_code_uri
+
+def create_experiment:
+    experiment = Experiment.create(
+        experiment_name = f"xgboost-target-direct-marketing-{suffix}", 
+        description = "Classification of target direct marketing", 
+        sagemaker_boto_client = sm)
+    )
+    return experiment
+
+def create_preprocessing_step(
+    execution_input,
+    bucket_name, 
+    input_code_uri,
+    data_file,
+    experiment_name
+):
+    preprocessing_processor = SKLearnProcessor(
+        framework_version='0.20.0',
+        role = role,
+        instance_count = 1,
+        instance_type = 'ml.m5.xlarge',
+        max_runtime_in_seconds = 1200
+    )
+
+    inputs = [
+        ProcessingInput(
+            input_name = "code",
+            source = input_code_uri,
+            destination = "/opt/ml/processing/input/code"
+        ),
+        ProcessingInput(
+            input_name = "input_data",
+            source = input_data,
+            destination='/opt/ml/processing/input'
+        )
+    ]
+
+    outputs = [
+        ProcessingOutput(
+            output_name = "train_data",
+            source = "/opt/ml/processing/output/train",
+            destination = f"{processing_output_data}/train"
+        ),
+        ProcessingOutput(
+            output_name = "validation_data",
+            source = "/opt/ml/processing/output/validation",
+            destination = f"{processing_output_data}/validation"
+        ),
+        ProcessingOutput(
+            output_name = "test_data",
+            source = "/opt/ml/processing/output/test",
+            destination = f"{processing_output_data}/test"
+        )
+    ]
+
+    trial_name = f"xgb-processing-job-{suffix}"
+    xgb_trial = Trial.create(
+        trial_name = trial_name, 
+        experiment_name = experiment_name,
+        sagemaker_boto_client = sm,
+    )
+    
+    processing_step = ProcessingStep(
+        "Preprocessing",
+        processor = preprocessing_processor,
+        job_name = execution_input["PreprocessingJobName"],
+        inputs = inputs,
+        outputs = outputs,
+        container_arguments = ["--data-file", data_file],
+        container_entrypoint = ["python3", "/opt/ml/processing/input/code/preprocessing.py"],
+        experiment_config = {
+            "TrialName": xgb_trial.trial_name,
+            "TrialComponentDisplayName": "Processing",
+        }
+    )    
+
+    return processing_step
+    
+def create_hpo_step(
+    execution_input, 
+    image_uri, 
+    bucket_name, 
+    ml_instance_count = 1,
+    ml_instance_type = 'ml.m5.xlarge',
+    objective_metric_name = 'validation:auc'
+):
+    tuning_output_path = f's3://{bucket_name}/tuning/output'
+
+    tuning_estimator = sagemaker.estimator.Estimator(
+        image_uri,
+        role, 
+        instance_count = ml_instance_count, 
+        instance_type = ml_instance_type,
+        output_path = tuning_output_path,
+        sagemaker_session = sagemaker_session
+    )    
+    hpo = dict(
+        max_depth = 5,
+        eta = 0.2,
+        gamma = 4,
+        min_child_weight = 6,
+        subsample = 0.8,
+        silent = 0,
+        objective = 'binary:logistic',
+        num_round = 100
+    ) 
+    tuning_estimator.set_hyperparameters(**hpo)
+    
+    hyperparameter_ranges = {
+        'eta': ContinuousParameter(0, 1),
+        'min_child_weight': ContinuousParameter(1, 10),
+        'alpha': ContinuousParameter(0, 2),
+        'max_depth': IntegerParameter(1, 10)
+    }
+    hpo_tuner = HyperparameterTuner(
+        tuning_estimator,
+        objective_metric_name,
+        hyperparameter_ranges,
+        max_jobs = 20,
+        max_parallel_jobs = 3
+    )
+
+    s3_input_train = TrainingInput(s3_data = f'{processing_output_data}/train', content_type = 'csv')
+    s3_input_validation = TrainingInput(s3_data = f'{processing_output_data}/validation', content_type = 'csv')
+    hpo_data = dict(
+        train = s3_input_train,
+        validation = s3_input_validation
+    )
+
+    # as long as HPO is selected, wait for completion.
+    tuning_step = TuningStep(
+        "HPO Step",
+        tuner = hpo_tuner,
+        job_name = execution_input["TuningJobName"],
+        data = hpo_data,
+        wait_for_completion = True
+    )
+
+    return tuning_step
+
+def create_lambda_query_hpo_job_step():
+    query_hpo_job_lambda_step = LambdaStep(
+        'Query HPO Job',
+        parameters = {  
+            "FunctionName": execution_input['LambdaFunctionNameOfQueryHpoJob'],
+            'Payload':{
+                "HpoJobName.$": "$$.Execution.Input['TuningJobName']"
+            }
+        }
+    )   
+    return query_hpo_job_lambda_step
+
+def create_hpo_job_sns_notification_step():
+    hpo_job_sns_step = SnsPublishStep(
+        state_id = 'SNS Notification - HPO Job',
+        parameters = {
+            'TopicArn': topic_arn,
+            'Message': query_hpo_job_lambda_step.output()['Payload']['bestTrainingJob']
+        }
+    )    
+    return hpo_job_sns_step
+
+def create_training_step(
+    execution_input, 
+    image_uri, 
+    bucket_name, 
+    experiment_name,
+    ml_instance_count = 1,
+    ml_instance_type = 'ml.m5.xlarge',
+):
+    training_output_path = f's3://{bucket_name}/training/output'
+    training_estimator = sagemaker.estimator.Estimator(
+        image_uri,
+        role, 
+        instance_count = ml_instance_count, 
+        instance_type = ml_instance_type,
+        output_path = training_output_path,
+        sagemaker_session = sagemaker_session
+    )
+        
+    hpo = dict(
+        max_depth = 5,
+        eta = 0.2,
+        gamma = 4,
+        min_child_weight = 6,
+        subsample = 0.8,
+        silent = 0,
+        objective = 'binary:logistic',
+        num_round = 100
+    )
+    training_estimator.set_hyperparameters(**hpo) 
+    
+    # use all the features for training.
+    s3_input_train = sagemaker.inputs.TrainingInput(s3_data=f'{output_data}/train', content_type='csv')
+    s3_input_validation = sagemaker.inputs.TrainingInput(s3_data=f'{output_data}/validation', content_type='csv')
+
+    training_data = dict(
+        train = s3_input_train,
+        validation = s3_input_validation
+    )
+
+    trial_name = f"xgb-training-job-{int(time.time())}"
+    xgb_trial = Trial.create(
+        trial_name = trial_name, 
+        experiment_name = experiment_name,
+        sagemaker_boto_client = sm,
+    )
+
+    training_step = TrainingStep(
+        "Model Training",
+        estimator = training_estimator,
+        data = training_data,
+        job_name = execution_input["TrainingJobName"],
+        wait_for_completion = True,
+        experiment_config = {
+            "TrialName": xgb_trial.trial_name,
+            "TrialComponentDisplayName": "Training",
+        },
+    )    
+    return training_step
+
+def create_model_step(
+    execution_input, 
+    training_step
+):
+    model_step = ModelStep(
+        "Save Model",
+        model = training_step.get_expected_model(),
+        model_name = execution_input["ModelName"]
+    )
+    return model_step
+
+def create_existing_model_step(
+    execution_input, 
+    image_uri
+):
+    # for deploying existing model
+    existing_model_name = f"dm-model-{uuid.uuid1().hex}"
+    existing_model = Model(
+        model_data = EXISTING_MODEL_URI,
+        image_uri = image_uri,
+        role = role,
+        name = existing_model_name
+    )
+    existing_model_step = ModelStep(
+        "Existing Model",
+        model = existing_model,
+        model_name = execution_input["ModelName"]
+    )
+    return existing_model_step
+
+def create_endpoint_configurgation_step(execution_input, deployment_instance_type = 'ml.c5.xlarge'):
+    endpoint_config_step = EndpointConfigStep(
+        "Create Model Endpoint Config",
+        endpoint_config_name = execution_input["ModelName"],
+        model_name = execution_input["ModelName"],
+        initial_instance_count = 1,
+        instance_type = 'ml.m5.xlarge'
+    )
+    return endpoint_config_step
+
+def create_lambda_query_endpoint_step():
+    query_endpoint_lambda_step = LambdaStep(
+        'Query Endpoint Existence',
+        parameters = {  
+            "FunctionName": execution_input['LambdaFunctionNameOfQueryEndpoint'],
+            'Payload':{
+                "EndpointName.$": "$$.Execution.Input['EndpointName']"
+            }
+        }
+    )
+    return query_endpoint_lambda_step
+
+def create_endpoint_step(execution_input):
+    endpoint_creation_step = EndpointStep(
+        "Create Endpoint",
+        endpoint_name = execution_input["EndpointName"],
+        endpoint_config_name = execution_input["ModelName"],
+        update = False
+    )
+    return endpoint_creation_step
+
+def update_endpoint_step(execution_input):
+    endpoint_update_step = EndpointStep(
+        "Update Endpoint",
+        endpoint_name = execution_input["EndpointName"],
+        endpoint_config_name = execution_input["ModelName"],
+        update = True
+    )   
+    return endpoint_update_step
+
+def create_query_endpoint_deployment_lambda_step():
+    query_endpoint_deployment_lambda_step = LambdaStep(
+        'Query Endpoint Deployment Status',
+        parameters = {  
+            "FunctionName": execution_input['LambdaFunctionNameOfQueryEndpoint'],
+            'Payload':{
+                "EndpointName.$": "$$.Execution.Input['EndpointName']"
+            }
+        }
+    )
+    return query_endpoint_deployment_lambda_step
+
+def create_check_endpoint_status_choice_step(
+    query_endpoint_lambda_step,
+    endpoint_update_step
+):
+    check_endpoint_status_step = Choice('Endpoint is InService?')
+
+    endpoint_in_service_rule = ChoiceRule.StringEquals(variable = query_endpoint_lambda_step.output()['Payload']['endpoint_status'], value = 'InService')
+    check_endpoint_status_step.add_choice(rule = endpoint_in_service_rule, next_step = endpoint_update_step)
+
+    wait_step = Wait(state_id = f"Wait Until Endpoint becomes InService", seconds = 20)
+    wait_step.next(query_endpoint_lambda_step)
+    check_endpoint_status_step.default_choice(next_step = wait_step)  
+
+    return check_endpoint_status_step  
+
+def create_check_endpoint_existence_choice_step(
+    query_endpoint_lambda_step,
+    check_endpoint_status_step,
+    endpoint_creation_step
+):
+    check_endpoint_existence_step = Choice('Endpoint Existed?')
+
+    endpoint_existed_rule = ChoiceRule.BooleanEquals(variable = query_endpoint_lambda_step.output()['Payload']['endpoint_existed'], value = True)
+    check_endpoint_existence_step.add_choice(rule = endpoint_existed_rule, next_step = check_endpoint_status_step)
+
+    check_endpoint_existence_step.default_choice(next_step = endpoint_creation_step)
+    return check_endpoint_existence_step
+
+def create_check_endpoint_is_deploying_choice_step(
+    query_endpoint_deployment_lambda_step
+):
+    # check endpoint readiness
+    deployed_endpoint_updating_step = Choice('Endpoint is deploying?')
+
+    wait_deployment_step = Wait(state_id = "Wait Until Deployment Is Completed...", seconds = 20)
+    wait_deployment_step.next(query_endpoint_deployment_lambda_step)
+
+    deployed_endpoint_updating_rule = ChoiceRule.StringEquals(variable = query_endpoint_deployment_lambda_step.output()['Payload']['endpoint_status'], value = 'Updating')
+    deployed_endpoint_updating_step.add_choice(rule = deployed_endpoint_updating_rule, next_step = wait_deployment_step)
+
+    final_step = Pass(state_id = 'Pass Step')
+    deployed_endpoint_updating_step.default_choice(next_step = final_step)
+
+    return deployed_endpoint_updating_step
+
+def create_to_do_hpo_choice_step(
+    tuning_step,
+    training_choice
+):
+    to_do_hpo_choice = Choice("To Do HPO?")
+
+    to_do_hpo_choice.add_choice(
+        rule = ChoiceRule.BooleanEquals(variable = "$$.Execution.Input['ToDoHPO']", value = True),
+        next_step = tuning_step                 
+    )
+    to_do_hpo_choice.add_choice(
+        rule = ChoiceRule.BooleanEquals(variable = "$$.Execution.Input['ToDoHPO']", value = False),
+        next_step = training_choice
+    )
+    return to_do_hpo_choice
+
+def create_to_do_training_choice_step(
+    training_path,
+    deploy_existing_model_path
+):
+    to_do_training_choice = Choice("To Do Model Training?")
+
+    to_do_training_choice.add_choice(
+        rule = ChoiceRule.BooleanEquals(variable = "$$.Execution.Input['ToDoTraining']", value = True),
+        next_step = training_path
+    )
+    to_do_training_choice.add_choice(
+        rule = ChoiceRule.BooleanEquals(variable = "$$.Execution.Input['ToDoTraining']", value = False),
+        next_step = deploy_existing_model_path
+    )
+    return to_do_training_choice
+
+def create_workflow(
+    region, 
+    bucket_name, 
+    workflow_name, 
+    workflow_execution_role
+):
+    
+    # Workflow Execution parameters
+    execution_input = ExecutionInput(
+        schema = {
+            "PreprocessingJobName": str,
+            "ToDoHPO": bool,
+            "ToDoTraining": bool,
+            "TrainingJobName": str,
+            "TuningJobName": str,
+            "ModelName": str,
+            "EndpointName": str,
+            "LambdaFunctionNameOfQueryEndpoint": str,
+            "LambdaFunctionNameOfQueryHpoJob": str
+        }
+    )
+    image_uri = sagemaker.image_uris.retrieve(region = region, framework='xgboost', version='latest')
+
+    # create the steps
+    tuning_step = create_hpo_step(execution_input, image_uri, bucket_name)
+    query_hpo_job_lambda_step = create_lambda_query_hpo_job_step()
+    hpo_job_sns_notification_step = create_hpo_job_sns_notification_step()
+    training_step = create_training_step(execution_input, image_uri, bucket_name)
+    model_step = create_model_step(execution_input, training_step)
+    existing_model_step = create_existing_model_step(execution_input, image_uri)
+    query_endpoint_lambda_step = create_lambda_query_endpoint_step()
+    endpoint_config_step = create_endpoint_configurgation_step(execution_input)
+    endpoint_creation_step = create_endpoint_step(execution_input)
+    endpoint_update_step = update_endpoint_step(execution_input)
+    query_endpoint_deployment_lambda_step = create_query_endpoint_deployment_lambda_step()
+
+    # create the choice steps
+    check_endpoint_status_choice_step = create_check_endpoint_status_choice_step(query_endpoint_lambda_step, endpoint_update_step)
+    check_endpoint_existence_choice_step = create_check_endpoint_existence_choice_step(
+        query_endpoint_lambda_step,
+        check_endpoint_status_choice_step,
+        endpoint_creation_step
+    )
+    check_endpoint_is_deploying_choice_step = create_check_endpoint_is_deploying_choice_step(
+        query_endpoint_deployment_lambda_step
+    )
+
+    query_endpoint_deployment_lambda_step.next(deployed_endpoint_updating_step)
+    endpoint_creation_step.next(query_endpoint_deployment_lambda_step)
+    endpoint_update_step.next(query_endpoint_deployment_lambda_step)
+    
+    training_path = Chain(
+        [
+            training_step, 
+            model_step, 
+            endpoint_config_step, 
+            query_endpoint_lambda_step, 
+            check_endpoint_existence_choice_step
+        ]
+    )
+    deploy_existing_model_path = Chain(
+        [
+            existing_model_step, 
+            endpoint_config_step, 
+            query_endpoint_lambda_step, 
+            check_endpoint_existence_choice_step
+        ]
+    )
+    tuning_step.next(query_hpo_job_lambda_step)
+    query_hpo_job_lambda_step.next(hpo_job_sns_step)
+
+    to_do_training_choice_step = create_to_do_training_choice_step(training_path, deploy_existing_model_path)
+    to_do_hpo_choice_step = create_to_do_hpo_choice_step(tuning_step, to_do_training_choice_step)
+
+    input_code_uri = upload_preprocess_code(bucket_name)
+    processing_step = create_preprocessing_step(execution_input, bucket_name, input_code_uri, data_file, experiment_name)
+
+    # catch execution exception
+    failed_state_sagemaker_pipeline_step_failure = Fail(
+        "ML Workflow Failed", cause = "SageMakerPipelineStepFailed"
+    )
+    catch_state_processing = Catch(
+        error_equals = ["States.TaskFailed"],
+        next_step = failed_state_sagemaker_pipeline_step_failure   
+    )
+    processing_step.add_catch(catch_state_processing)
+    tuning_step.add_catch(catch_state_processing)
+    training_step.add_catch(catch_state_processing)
+    model_step.add_catch(catch_state_processing)
+    endpoint_config_step.add_catch(catch_state_processing)
+    endpoint_step.add_catch(catch_state_processing)
+    existing_model_step.add_catch(catch_state_processing)
+    
+    workflow_graph = Chain([processing_step, hpo_choice])
+    workflow = Workflow(
+        name = workflow_name,
+        definition = workflow_graph,
+        role = workflow_execution_role
+    )
+    workflow.create()
+    return workflow
+    
+    
+def main(
+    workflow_name,
+    workflow_execution_role,
+    processing_repository_uri,
+    require_hpo,
+    require_model_training
+):
+    # bucket_name is created in ml_pipeline_dependencies.py, which is imported at the beginning.
+    workflow = create_workflow(region, bucket_name, workflow_name, workflow_execution_role, processing_repository_uri)
+
+    # execute workflow
+    preprocessing_job_name = f"aqf-preprocessing-{uuid.uuid1().hex}"
+    tuning_job_name = f"aqf-tuning-{uuid.uuid1().hex}"
+    training_job_name = f"aqf-training-{uuid.uuid1().hex}"
+    model_job_name = f"aqf-model-{uuid.uuid1().hex}"
+    endpoint_job_name = f"aqf-endpoint-{uuid.uuid1().hex}"
+    batch_transform_job_name = f"aqf-transform-{uuid.uuid1().hex}"
+    todoHPO = require_hpo.lower() in ['true', '1', 'yes', 't']
+    todoTraining = require_model_training.lower() in ['true', '1', 'yes', 't']
+    
+    execution = workflow.execute(
+        inputs = {
+            "PreprocessingJobName": preprocessing_job_name,
+            "ToDoHPO": todoHPO,
+            "ToDoTraining": todoTraining,
+            "TrainingJobName": training_job_name,
+            "TuningJobName": tuning_job_name,
+            "ModelName": model_job_name,
+            "EndpointName": endpoint_job_name,
+            "TransformJobName": batch_transform_job_name
+        }
+    )
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Load parameters")
+    parser.add_argument("--workflow-name", required=True)
+    parser.add_argument("--workflow-execution-role", required=True)
+    parser.add_argument("--processing-repository-uri", required=True)
+    parser.add_argument("--require-hpo", required=True)
+    parser.add_argument("--require-model-training", required=True)
+    args = vars(parser.parse_args())
+    print("args: {}".format(args))
+    main(**args)
---
+++ notebook/pipeline/ml_pipeline_dependencies.py
@@ -0,0 +1,38 @@
+import boto3
+import time
+import re
+import uuid
+from datetime import datetime
+
+import stepfunctions
+from stepfunctions.inputs import ExecutionInput
+from stepfunctions.steps.sagemaker import *
+from stepfunctions.steps.states import *
+from stepfunctions.steps.compute import *
+from stepfunctions.workflow import Workflow
+from stepfunctions.steps import *
+from IPython.display import display, HTML, Javascript
+
+import sagemaker
+from sagemaker import get_execution_role
+from sagemaker.processing import ProcessingInput, ProcessingOutput
+from sagemaker.sklearn.processing import SKLearnProcessor
+from sagemaker.tuner import IntegerParameter, CategoricalParameter, ContinuousParameter, HyperparameterTuner
+from sagemaker.analytics import ExperimentAnalytics
+from sagemaker.inputs import TrainingInput
+
+from smexperiments.experiment import Experiment
+from smexperiments.trial import Trial
+from smexperiments.trial_component import TrialComponent
+from smexperiments.tracker import Tracker
+from smexperiments.search_expression import Filter, Operator, SearchExpression
+
+session = boto3.Session()
+sm = session.client('sagemaker')
+sagemaker_session = sagemaker.Session()
+
+region = boto3.Session().region_name
+role = get_execution_role()
+bucket_name = f'xgboost-direct-marketing-{account_id}-{region}'
+prefix = 'sagemaker/DEMO-xgboost-dm/manual_pipeline'
+account_id = session.client('sts').get_caller_identity().get('Account')
